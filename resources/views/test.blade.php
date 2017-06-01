1607e3fcffe1a3e260a27d9c9bf6d64b969c86c8
Mykolas25 <justmikis@gmail.com>
2017-06-01 10:28:07 +0300

    test css commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Title of the document</title>
+    <style>
+        .highlight1, .highlight2{
+            color:red;
+        }
+        .niceFont{
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+
+<div id="first" style="color:green">The content of the document <span class="highlight1 niceFont">Textas spane</span></div>
+<div id="second" style="color:green">The content of the document <span class="highlight2 niceFont">Textas spane</span></div>
+</body>
+
+</html>
